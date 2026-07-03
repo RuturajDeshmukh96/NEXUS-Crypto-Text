cbc5c93f37bc364968745ec82212dc29c26f331d
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com@gmail.com>
2026-07-04 00:04:30 +0530

    fix: UI CSS loading on Vercel
    - Removed static folder routes from vercel.json so WhiteNoise intercepts the requests
    - Pointed STATIC_ROOT directly to the codebase static folder to bypass the need for collectstatic in serverless environments

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 # Disable compressed manifest to prevent 500 errors if collectstatic doesn't run properly
 WHITENOISE_MANIFEST_STRICT = False
 
